31968803d7806d5fd2e20bffe19882d0982b72f8
Noval Dwi Putra <nvldwiptr@gmail.com>
2020-06-10 16:39:35 +0700

    customize web.php

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,34 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', 'DashboardController@index');
+
+Route::prefix('master')->group(function () {
+    Route::resource('bank', 'Master\BankController')->except([
+        'show'
+    ]);
+    Route::resource('blood-type', 'Master\BloodTypeController')->except([
+        'show'
+    ]);
+    Route::resource('department', 'Master\DepartmentController')->except([
+        'show'
+    ]);
+    Route::resource('designation', 'Master\DesignationController')->except([
+        'show'
+    ]);
+    Route::resource('gender', 'Master\GenderController')->except([
+        'show'
+    ]);
+    Route::resource('job-status', 'Master\JobStatusController')->except([
+        'show'
+    ]);
+    Route::resource('marital', 'Master\MaritalController')->except([
+        'show'
+    ]);
+    Route::resource('religion', 'Master\ReligionController')->except([
+        'show'
+    ]);
+    Route::resource('taxes', 'Master\TaxesController')->except([
+        'show'
+    ]);
 });
